0b15e3fb6a226508db95ae8d8304c4dbfc84b65f
Jared Wiese <7630902+jaredwiese@users.noreply.github.com>
2021-09-07 22:15:24 -0500

    remove admin page

--- crypto/settings.py
+++ crypto/settings.py
@@ -35,7 +35,6 @@ ADMIN_ENABLED = False
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
